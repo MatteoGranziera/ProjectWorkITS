4e10416eb491b47f09bc67f840584799c739d69b
Blue_Lama <alessiodeluca95@gmail.com>
2015-05-19 11:31:58 +0200

    -modified .txt to .sql -modifided score table: added month

--- Databases/WebDB/query projectwork.sql
+++ Databases/WebDB/query projectwork.sql
@@ -1,4 +1,4 @@
-Creare la tabella stati
+#Creare la tabella stati
 
 
 CREATE SEQUENCE sequence_country START 1;
@@ -8,7 +8,7 @@ nome varchar(30),
 PRIMARY KEY (id));
 
 
-Creare la tabella linguaggi
+#Creare la tabella linguaggi
 
 CREATE SEQUENCE sequence_languages START 1;
 CREATE TABLE languages (
@@ -18,7 +18,7 @@ PRIMARY KEY (id));
 
 
 
-Creare la tabella punteggio
+#Creare la tabella punteggio
 
 
 CREATE SEQUENCE sequence_score START 1;
@@ -26,6 +26,7 @@ CREATE TABLE score (
 id_country integer,
 id_language integer,
 score integer,
+month date, #first day of the month
 PRIMARY KEY (id_country, id_language),
 FOREIGN KEY(id_country) REFERENCES country(id),
 FOREIGN KEY(id_language) REFERENCES languages(id));
