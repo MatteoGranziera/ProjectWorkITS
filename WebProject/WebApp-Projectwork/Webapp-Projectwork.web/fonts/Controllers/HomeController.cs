46eb8eca08d6a51c452d72f78b2e189e799a2560
Blue_Lama <alessiodeluca95@gmail.com>
2015-05-20 10:04:50 +0200

    Changed Home views and layout configuration

--- WebProject/WebApp-Projectwork/Webapp-Projectwork.web/fonts/Controllers/HomeController.cs
+++ WebProject/WebApp-Projectwork/Webapp-Projectwork.web/fonts/Controllers/HomeController.cs
@@ -18,16 +18,16 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Aboutus()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult AboutITSKennedy()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
