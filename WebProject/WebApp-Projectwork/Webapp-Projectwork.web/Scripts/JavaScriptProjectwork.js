d71a4a3412234a111ee7e4fb1aa3c81350b2ac54
Blue_Lama <alessiodeluca95@gmail.com>
2015-05-21 12:01:41 +0200

    Added javascriptprojectwork, stylesheetprojectwork and javascript code

---
+++ WebProject/WebApp-Projectwork/Webapp-Projectwork.web/Scripts/JavaScriptProjectwork.js
@@ -0,0 +1,15 @@
+﻿var provadati = $('#provadati')
+$(function () {
+    $("#map-europe ul li").each(function () {
+        $(this).mouseenter(function () {
+            provadati.text('cieo');
+        });
+    });
+});
+$(function () {
+    $("#map-europe ul li").each(function () {
+        $(this).mouseleave(function () {
+            provadati.text('');
+        });
+    });
+});
