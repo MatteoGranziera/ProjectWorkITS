c8baf5289ce4a93fbfaa7892192eacc9871446ba
Matteo <granziera@gmail.com>
2015-05-28 10:35:32 +0200

    - Implemented Tweet Class to return objects from JSON

--- DataAnalyser/com.tsac.projectwork.dataanalyser/src/com/tsac/projectwork/dataanalyser/data/Tweet.java
+++ DataAnalyser/com.tsac.projectwork.dataanalyser/src/com/tsac/projectwork/dataanalyser/data/Tweet.java
@@ -1,5 +1,9 @@
 package com.tsac.projectwork.dataanalyser.data;
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Dictionary;
+import java.util.List;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,10 +15,17 @@ public class Tweet {
 	private String KEY_RETWITTED = "retweeted";
 	private String KEY_ENTITIES = "hashtags";
 	private String KEY_RETWEET_COUNT = "retweet_count";
+	private String KEY_COUNTRY = "country";
 	
+	//Utils Strings
 	private String NULL_STRING = "NaN";
+	private String TWITTER_DATE="EEE, dd MMM yyyy HH:mm:ss ZZZZZ";
 	
 	private JSONObject jsonObj;
+	
+	public Tweet(String json) throws JSONException{
+		ParseJSON(json);
+	}
 	
 	public void ParseJSON(String json) throws JSONException{
 		jsonObj = new JSONObject(json);
@@ -27,9 +38,39 @@ public class Tweet {
 		return NULL_STRING;
 	}
 	
-	public Date getCreation() throws JSONException{
+	public Date getCreation() throws JSONException, ParseException{
 		if(jsonObj != null){
-			return jsonObj.get(KEY_TEXT);
+			SimpleDateFormat sf = new SimpleDateFormat(TWITTER_DATE);
+			return new Date(sf.parse(jsonObj.getString(KEY_CREATION)).getTime());
+		}
+		return null;
+	}
+	
+	public boolean getRetweeted() throws JSONException{
+		if(jsonObj != null){
+			return  jsonObj.getBoolean(KEY_RETWITTED);
+		}
+		return false;
+	}
+	
+	public int getNumRetweet() throws JSONException{
+		if(jsonObj != null){
+			return jsonObj.getInt(KEY_RETWEET_COUNT);
+		}
+		return -1;
+	}
+	
+	public List<String> getEntities() throws JSONException{
+		if(jsonObj != null){
+			//  jsonObj.getString(KEY_RETWITTED);
+			return null;
+		}
+		return null;
+	}
+	
+	public String getCountry() throws JSONException{
+		if(jsonObj != null){
+			return  jsonObj.getString(KEY_COUNTRY);
 		}
 		return NULL_STRING;
 	}
