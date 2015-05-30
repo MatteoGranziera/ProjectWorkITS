877fa3ddee99257c9245c3555a833854dc196d45
Matteo <granziera@gmail.com>
2015-05-30 20:36:47 +0200

    removed unsed import

--- DataAnalyser/com.tsac.projectwork.dataanalyser/src/com/tsac/projectwork/dataanalyser/config/ConfigManager.java
+++ DataAnalyser/com.tsac.projectwork.dataanalyser/src/com/tsac/projectwork/dataanalyser/config/ConfigManager.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import com.google.common.base;
 
 import com.tsac.projectwork.dataanalyser.DbWriter;
 
