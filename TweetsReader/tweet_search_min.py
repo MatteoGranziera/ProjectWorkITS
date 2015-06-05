794c79de5d02188bc8b5e9298dbe1c021c2fb874
nics95 <n.nicolatedesco@gmail.com>
2015-06-05 09:22:24 +0200

    Updated

--- TweetsReader/tweet_search_min.py
+++ TweetsReader/tweet_search_min.py
@@ -1,110 +1,38 @@
 #!/usr/bin/python3
-'''
-TODO:
-    create new dict from tweets, no deleting useless keys
-'''
-
-
-'''
-Module used to:
-    connect with twitter APIs and get tweets
-    clean tweets from it's useless parts
-    push tweets in a Redis queue
-
-Requires Twitter APP credentials stored in the following env vars:
-  LOGENTRIES_TOKEN
-  TWITTER_API_PKEY
-  TWITTER_API_SECRET
-'''
-# standard library
-import base64
-import json
-
-# 3rd party
-import requests
-import redis
-
-def set_logger(logentries_token):
-    '''
-    Parameters:
-        logentries_token: logentries access token
-    Returns:
-        log: logger
-    '''
-    log = logging.getLogger('logentries')
-    log.setLevel(logging.INFO)
-    log.addHandler(LogentriesHandler(logentries_token))
-    return log
-
+import base64, json, requests, redis
 def get_db_datas(choice):
-    '''
-    Parameters:
-        choice: 'states' or 'languages'
-    Returns:
-        data: data we need
-    '''
-    conn = psycopg2.connect(user='projectwork', password='password!',
-                            dbname='projectwork', host='54.72.51.122',
-                            port=5432)
-    curs = conn.cursor()
-    if choice == 'states':
-        curs.execute(
-            'SELECT name, id_state_twitter FROM countries'
-            )
-        data = {}
-        for chunk in curs.fetchall():
-            data[chunk[0]] = chunk[1]
-    '''
-    elif choice == 'languages':
-        data = []
-        curs.execute(
-            'SELECT name FROM languages'
-            )
-        data = curs.fetchall()
-    '''
-    conn.commit()
-    curs.close()
-    conn.close()
-    return data
-    
+    co=psycopg2.connect(user='projectwork',password='password!',dbname='projectwork',host='54.72.51.122',port=5432)
+    cu=conn.cursor()
+    if choice=='states':
+        cu.execute('SELECT name, id_state_twitter FROM countries')
+        d={}
+        for c in cu.fetchall():
+            d[c[0]]=c[1]
+    elif choice=='languages':
+        d=[]
+        cu.execute('SELECT name FROM languages')
+        d=cu.fetchall()
+    co.commit()
+    cu.close()
+    co.close()
+    return d
 def get_token(consumer_key, secret_key):
-    '''
-    Parameters:
-        consumer_key: public key of my Twitter applications
-        secret_key: secret key of my Twitter applications
-    Returns:
-        a token we need to authenticate requests to Twitter APIs
-    '''
-    credentials = '{}:{}'.format(consumer_key, secret_key)
-    credentials_enc = base64.b64encode(credentials.encode())
-    url = 'https://api.twitter.com/oauth2/token'
-    ctype = 'application/x-www-form-urlencoded;charset=UTF-8'
-    resp = requests.post(
-        url,
-        headers={'Authorization': 'Basic {}'.format(credentials_enc.decode()), 'Content_type':ctype.encode()},
-        data={'grant_type': 'client_credentials'}
-    )
-    resp.raise_for_status()
-    resp = resp.json()
-    return resp['access_token']
-
+    cr='{}:{}'.format(consumer_key, secret_key)
+    cre=base64.b64encode(cr.encode())
+    re=requests.post('https://api.twitter.com/oauth2/token'headers={'Authorization': 'Basic {}'.format(cre.decode()), 'Content_type':'application/x-www-form-urlencoded;charset=UTF-8'.encode()},data={'grant_type': 'client_credentials'})
+    re.raise_for_status()
+    re=re.json()
+    return re['access_token']
 def get_tweets(token, conn):
-    '''
-    Parameters:
-        word: the query to make to search API
-        token: the key we need to authenticate requests
-    Returns:
-        a list of tweets (dictionaries) as returned by twitter API
-    '''
-    states = get_db_datas('states')
-    languages = get_db_datas('languages')
-
-    x = 0
-    for state in states.values():
-        x += 1
+    st=get_db_datas('states')
+    lan=get_db_datas('languages')
+    la = ''
+    for l in lan:
+        la+='"'+l+'"'(' OR')
+    for s in st.values():
         resp = requests.get(
                             'https://api.twitter.com/1.1/search/tweets.json',
-                            params={'q':'place:{} {}'.format(state, languages), 'count':100},
+                            params={'q':'place:{} {}'.format(s, lan), 'count':100},
                             headers={'Authorization': 'Bearer {}'.format(token)}
                             )
         resp.raise_for_status()
@@ -147,6 +75,8 @@ def clean_tweets(tweets):
                 new[word] = tweet[word]
             if tweet['place']['country'] != None:
                 new['state'] = tweet['place']['country']
+            #if int(['entities']['hashtags']) != None:
+            #    new['hashtags'] = tweet['entities']['hashtags']
         print(new)
         cleaned.append(new)
     return cleaned
@@ -183,7 +113,9 @@ if __name__ == '__main__':
     import logging
     from logentries import LogentriesHandler
     import psycopg2
-    log = set_logger(os.getenv('PROJECTWORK_LOGENTRIES_TOKEN'))
+    log = logging.getLogger('logentries')
+    log.setLevel(logging.INFO)
+    log.addHandler(LogentriesHandler(os.getenv('PROJECTWORK_LOGENTRIES_TOKEN')))
     log.info('Started')
     log.info('getting bearer token')
     token = get_token(
