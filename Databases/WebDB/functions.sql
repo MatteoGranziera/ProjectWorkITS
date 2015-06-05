73e52c6dc4b9bf8129d9d676122fc44c4cba95dd
Matteo <granziera@gmail.com>
2015-06-05 20:52:40 +0200

    Added query to create function to addscore in postgresql

---
+++ Databases/WebDB/functions.sql
@@ -0,0 +1,39 @@
+﻿CREATE FUNCTION addscore (varchar,varchar, integer, date) RETURNS boolean AS '
+DECLARE
+-- Declare aliases for function arguments.
+lang ALIAS FOR $1;
+count ALIAS FOR $2;
+sc ALIAS FOR $3;
+
+  id_l INTEGER;
+  id_c INTEGER;
+  id_s INTEGER;
+  mo DATE;
+
+BEGIN
+
+  SELECT INTO id_l id FROM languages WHERE
+    name = lang;
+
+  IF id_l IS NULL THEN
+    RETURN FALSE;
+  END IF;
+
+  SELECT INTO id_c id FROM countries WHERE name=count;
+
+  IF id_c IS NULL THEN
+    RETURN FALSE;
+  END IF;
+
+  SELECT INTO id_s id FROM scores WHERE id_language = id_l AND id_country = id_c;
+
+  IF id_s IS NULL THEN
+    INSERT INTO scores(id_country, id_language, score, month) VALUES ( id_c, id_l, sc, mo );
+    SELECT INTO id_s id FROM scores WHERE id_language = id_l AND id_country = id_c;
+  END IF;
+
+  UPDATE scores SET score = score + sc WHERE id = id_s;
+  RETURN TRUE;
+
+END;
+' LANGUAGE 'plpgsql';
